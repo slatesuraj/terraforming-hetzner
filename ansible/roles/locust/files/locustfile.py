7e4dae2e59f01275b49c126baccef8d905fc3817
Suraj Dhakre <sdctkhs@gmail.com>
2023-09-03 16:14:30 +0530

    Creating symlink for locustfile

--- ansible/roles/locust/files/locustfile.py
+++ ansible/roles/locust/files/locustfile.py
@@ -1,13 +1,1 @@
-from locust import HttpUser, between, task
-
-
-class WebsiteUser(HttpUser):
-    wait_time = between(1, 2)
-
-    @task
-    def home(self):
-        self.client.get("/")
-
-    @task
-    def contact(self):
-        self.client.get("/de/kontakt")
+../../../../locust/locustfile.py
